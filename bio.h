d426400c8c0c03366d3b5fd8079c72a6d6e154aa
Regina <laulessr@gmail.com>
2015-01-09 17:57:09 -0600

    Updated bio.h a second time for step 8.  Added more bio info.

--- bio.h
+++ bio.h
@@ -5,5 +5,11 @@ the project I am working on the best.  It's a music composition written in
 Clojure using Overtone.
 It's a lot of fun, even if the song is still a major work in progress. :)
 
+My favorite project in 161/162 was my final project in 162.  It was a 
+Pride & Prejudice text game.
 
+I enjoyed it because it was a non-violent video game.  I don't care for 
+violence in my hobbies.  Maybe that's why music, knitting, and programming
+are my main hobbies!  
+  
 
