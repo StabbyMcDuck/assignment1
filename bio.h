672f285ae1b6a8a136747e8e75a0a9f0958650f8
Regina <laulessr@gmail.com>
2015-01-09 17:51:02 -0600

    Updated bio.h with more information as per step 8 first commit

--- bio.h
+++ bio.h
@@ -1,1 +1,9 @@
-Programming Background
+Programming Background:
+I have tried Ruby and Clojure and some Pythong in the past.
+I most enjoy working in Clojure, but I believe that's because I enjoy
+the project I am working on the best.  It's a music composition written in
+Clojure using Overtone.
+It's a lot of fun, even if the song is still a major work in progress. :)
+
+
+
